f5f8b694b334b0c61237fc862caaaf822f65ec81
jcbecker <joaocarlosbecker666@gmail.com>
2017-05-22 16:18:25 -0300

    fazendo formulario

--- js/init.js
+++ js/init.js
@@ -1,7 +1,14 @@
 (function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
+    $(".button-collapse").sideNav();
 })(jQuery); // end of jQuery name space
+
+
+    
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
